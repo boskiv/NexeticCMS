78a8dcde68e6bf87b3554e687c84e7685c0762c2
Ivan Skiridomov <ivan.skiridomov@nexetic.com>
2014-10-29 16:47:01 +0200

    Clean Updated MVC5 App

---
+++ NexeticCMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NexeticCMS.Startup))]
+namespace NexeticCMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
